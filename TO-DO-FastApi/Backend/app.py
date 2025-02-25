8a04bfc9ac13869eed595c21ce03b986487125c2
Anurag Dubey <129617718+AnuragDubey14@users.noreply.github.com>
2025-02-25 17:48:30 +0530

    Update app.py

--- TO-DO-FastApi/Backend/app.py
+++ TO-DO-FastApi/Backend/app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI 
 from routes.routes import taskRouter
-
-# Import the function to create tables
-from config.db import create_db # This should match the function name in db.py
+from config.db import create_db 
 
 # Call create_db() to create tables on app startup
 create_db()
